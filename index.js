2ffb5e6e88c5ba06b36e09a644784acf22439ddc
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-22 15:22:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector); 
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n);
+  }
+}
+
+function deepestChild() {
+  var child = document.getElementById('grand-node');
+  if (child.querySelector('div')) {
+      deepestChild(child.querySelector('div').innerHTML);
+  }
+   else {
+       return child.innerHTML;
+  }
+}

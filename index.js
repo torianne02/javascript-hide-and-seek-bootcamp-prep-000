6aa9104e60008bb2b5d6df84ffb35d11be7668e2
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-22 15:59:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function increaseRankBy(n) {
 function deepestChild() {
   var all = document.getElementById("grand-node");
   var child = all.querySelector('div div div div div');
-  return child.innerHTML;
+  return 'boo';
 }
